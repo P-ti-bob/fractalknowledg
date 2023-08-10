debce8845b17fd2bd2717415045fd7c70b936304
P-ti-bob <81181827+P-ti-bob@users.noreply.github.com>
2023-08-10 11:35:03 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Optionnel : Ajouter une interaction pour masquer/dévoiler les détails des services
-const services = document.querySelectorAll('.service');
-
-services.forEach(service => {
-    service.addEventListener('click', () => {
-        service.classList.toggle('expanded');
-    });
-});
